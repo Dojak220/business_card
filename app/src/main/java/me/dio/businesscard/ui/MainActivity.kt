aa25a01ea4a128bfb9b27340c1300a1666d19a17
Dorgival Rocha <dojak220@ufrn.edu.br>
2022-08-08 22:45:20 -0300

    using bindingView

--- app/src/main/java/me/dio/businesscard/ui/MainActivity.kt
+++ app/src/main/java/me/dio/businesscard/ui/MainActivity.kt
@@ -3,10 +3,14 @@ package me.dio.businesscard.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import me.dio.businesscard.R
+import me.dio.businesscard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
