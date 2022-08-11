79a525a320b39b91538448306efa2f2194fcf736
Dorgival Rocha <dojak220@ufrn.edu.br>
2022-08-11 00:11:04 -0300

    Add recycler view and business card components

--- app/src/main/java/me/dio/businesscard/ui/MainActivity.kt
+++ app/src/main/java/me/dio/businesscard/ui/MainActivity.kt
@@ -6,11 +6,10 @@ import me.dio.businesscard.R
 import me.dio.businesscard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
 
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 }
