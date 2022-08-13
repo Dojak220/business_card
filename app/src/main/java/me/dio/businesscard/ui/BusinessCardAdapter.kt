8cece3701c94b0c689992984a0c9ba657d270406
Dorgival Rocha <dojak220@ufrn.edu.br>
2022-08-13 15:34:12 -0300

    Connects insert and get business cards to db

---
+++ app/src/main/java/me/dio/businesscard/ui/BusinessCardAdapter.kt
@@ -0,0 +1,60 @@
+package me.dio.businesscard.ui
+
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import me.dio.businesscard.data.BusinessCard
+import me.dio.businesscard.databinding.BusinessCardItemBinding
+
+
+class BusinessCardAdapter :
+    ListAdapter<BusinessCard, BusinessCardAdapter.ViewHolder>(DiffCallback()) {
+
+    val listenerShare: (View) -> Unit = {}
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = BusinessCardItemBinding.inflate(inflater, parent, false)
+
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val current = getItem(position)
+        holder.bind(current)
+    }
+
+    inner class ViewHolder(private val binding: BusinessCardItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: BusinessCard) {
+            binding.tvName.text = item.name
+            binding.tvCompanyName.text = item.company
+            binding.tvPhone.text = item.phone
+            binding.tvEmail.text = item.email
+            binding.cvBusinessCard.setCardBackgroundColor(Color.parseColor(item.backgroundColor))
+
+            binding.cvBusinessCard.setOnClickListener {
+                listenerShare(it)
+            }
+        }
+
+//        companion object {
+//            fun create(parent: ViewGroup): WordViewHolder {
+//                val view: View = LayoutInflater.from(parent.context)
+//                    .inflate(R.layout.recyclerview_item, parent, false)
+//                return WordViewHolder(view)
+//            }
+//        }
+    }
+}
+
+class DiffCallback : DiffUtil.ItemCallback<BusinessCard>() {
+    override fun areItemsTheSame(oldItem: BusinessCard, newItem: BusinessCard) = oldItem == newItem
+    override fun areContentsTheSame(oldItem: BusinessCard, newItem: BusinessCard) =
+        oldItem.id == newItem.id
+}
--- app/src/main/java/me/dio/businesscard/ui/MainActivity.kt
+++ app/src/main/java/me/dio/businesscard/ui/MainActivity.kt
@@ -1,14 +1,39 @@
 package me.dio.businesscard.ui
 
+import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import me.dio.businesscard.App
 import me.dio.businesscard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val mainViewModel: MainViewModel by viewModels {
+        MainViewModelFactory((application as App).repository)
+    }
+
+    private val adapter by lazy { BusinessCardAdapter() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        binding.rvBusinessCards.adapter = adapter
+        getAllBusinessCards()
+        insertListeners()
+    }
+
+    private fun insertListeners() {
+        binding.fabAddCardButton.setOnClickListener {
+            val intent = Intent(this@MainActivity, AddBusinessCardActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun getAllBusinessCards() {
+        mainViewModel.getALl().observe(this) { businessCard ->
+            adapter.submitList(businessCard)
+        }
     }
 }
